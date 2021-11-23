9b368371351ae7c3b8aab43502ac5008ec9b0406
alexno-sherlock <alexno@stanford.edu>
2021-11-23 04:56:19 -0800

    add script to convert pickles into csv

---
+++ pickle_to_csv.py
@@ -0,0 +1,54 @@
+import argparse
+import time
+from pathlib import Path
+from tqdm import tqdm
+
+import pandas as pd
+
+from src.utils.pickle_reader import read_pickle
+
+
+def convert_to_csv(filepath):
+    predictions, targets = read_pickle(filepath)
+    df = pd.concat(
+        [pd.DataFrame(targets, columns=["Hypnogram"]), pd.DataFrame(predictions, columns=["W", "N1", "N2", "N3", "R"])],
+        axis=1,
+    )
+    df.to_csv(filepath.parent / (filepath.stem + ".csv"), index=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--data_dir", type=str, help="Path to directory containing .pkl files to convert.")
+    parser.add_argument("-f", "--file", type=str, help="Path to specific file to convert.")
+    args = parser.parse_args()
+
+    assert (args.data_dir is not None and args.file is None) or (
+        args.data_dir is None and args.file is not None
+    ), f"Specify either a data directory or a file, received data_dir={args.data_dir} and file={args.file}"
+
+    if args.data_dir is not None:
+        data_dir = Path(args.data_dir)
+        list_files = sorted(list(data_dir.glob("**/*.pkl")))
+        # N = len(list_files)
+        N = 10
+        list_files = list_files[:N]
+        if N == 0:
+            print(f"No .pkl files found in directory!")
+        else:
+            print(f"Starting conversion of {N} .pkl files...")
+            bar = tqdm(list_files)
+            start = time.time()
+            for filepath in bar:
+                bar.set_description(filepath.stem)
+                convert_to_csv(filepath)
+            end = time.time()
+            print(f"Finished, {N} files converted in {end-start} seconds.")
+    elif args.file is not None:
+        N = 1
+        filepath = Path(args.file)
+        print(f"Converting file {filepath.stem}...")
+        start = time.time()
+        convert_to_csv(filepath)
+        end = time.time()
+        print(f"Converted {filepath.stem} in {end-start} seconds.")
--- src/utils/pickle_reader.py
+++ src/utils/pickle_reader.py
@@ -3,9 +3,8 @@ import pickle
 import numpy as np
 
 
-def read_pickle(filepath):
-    '''Read contents of the output pickle files.
-    
+def read_pickle(filepath: str):
+    """Read contents of output pickle files.
     Usage:
     from pickle_reader import read_pickle
     
@@ -17,16 +16,17 @@ def read_pickle(filepath):
     Returns:
         y: ndarray -- output predictions every second
         t: ndarray -- targets every second (hypnogram labels)
-    '''
+    """
+
     # This loads contents of pickle file into a dict
-    with open(filepath, 'rb') as pkl:
+    with open(filepath, "rb") as pkl:
         out = pickle.load(pkl)
 
     # Isolate and reshape targets from (N, 300) into (N * 300,)
-    targets = np.asarray(out['targets']).reshape(-1)
+    targets = np.asarray(out["targets"]).reshape(-1)
 
     # Isolate and reshape predictions from (N, 5, 300) into (N * 300, 5)
-    predictions = np.asarray(out['predictions'])
+    predictions = np.asarray(out["predictions"])
     N, K, T = predictions.shape
     predictions = predictions.transpose(1, 0, 2).reshape(K, N * T).T
 
